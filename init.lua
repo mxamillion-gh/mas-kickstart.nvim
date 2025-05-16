5b9d1eb212518faf1bf2ee7623bc416e073b27e6
mxamillion-gh <emailvonmax@mailbox.org>
2025-05-16 13:06:08 +0200

    Added pyright settings to detect .venv

--- init.lua
+++ init.lua
@@ -686,10 +686,26 @@ require('lazy').setup({
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+      local util = require 'lspconfig.util'
+
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = {},
+        pyright = {
+          settings = {
+            root_dir = util.root_pattern 'pyproject.toml',
+            python = {
+              venvPath = '.',
+              venv = '.venv',
+              analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                diagnosticMode = 'openFilesOnly',
+                typeCheckingMode = 'basic',
+              },
+            },
+          },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
