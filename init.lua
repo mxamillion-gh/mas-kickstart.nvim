ebd20ba76b8dbb0fdc1bf4277c7fd70d28e25f11
mxamillion-gh <emailvonmax@mailbox.org>
2025-09-08 13:45:26 +0200

    Added hidden files to pickers

--- init.lua
+++ init.lua
@@ -428,7 +428,12 @@ require('lazy').setup({
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
-        -- pickers = {}
+        pickers = {
+          find_files = {
+            hidden = true,
+            no_ignore = true,
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
