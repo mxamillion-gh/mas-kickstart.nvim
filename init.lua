9e4ab94b9a98150b1facb1dcb839a20a9a381ce4
mxamillion-gh <emailvonmax@mailbox.org>
2025-11-01 21:28:57 +0100

    Added hidden files option

--- init.lua
+++ init.lua
@@ -411,7 +411,9 @@ require('lazy').setup({
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
-        -- pickers = {}
+        pickers = {
+          find_files = { hidden = true },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
